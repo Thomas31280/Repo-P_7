59b40d482bd1f13ef1cdde273532b4d7b2774e69
Thomas Duval <tomas1905@hotmail.fr>
2021-07-21 17:57:29 +0200

    Adding of all python's process methods

--- utils.py
+++ utils.py
@@ -1,11 +1,71 @@
 import googlemaps
-from datetime import datetime
+import wikipedia
 
 import config
 
+
+# Set an instance of Google Maps
 gmaps = googlemaps.Client(key=config.MAPS_API_KEY)
+wikipedia.set_lang("fr")
 
-# find a place with an address
-find_place_result = gmaps.find_place('1600 Amphitheatre Parkway, Mountain View, CA', 'textquery')
+class Process:
 
-print(find_place_result)
+    @classmethod
+    def parser(cls, userquestion):
+
+        words_to_ignore = ["Salut", "GrandPy", "!", "Est-ce", "que", "tu", "connais", "l'adresse", "?"]
+        output_value = ''
+
+        for word in userquestion.split():
+
+            if word in words_to_ignore:
+                pass
+            else:
+                output_value += (word + ' ')
+        
+        return output_value
+        
+
+    @classmethod
+    def google_maps_API(cls, question_parsed):
+        
+        try:
+            # Geocoding the parsed adress
+            geocode_result = gmaps.geocode(question_parsed)
+            response = {"coordinates": geocode_result[0]['geometry']['location'], 
+                        "adress": geocode_result[0]['formatted_address']}
+
+            return response
+        
+        except:
+            # In case of failure
+            return "ERROR : This adress couldn't been find..."
+    
+
+    @classmethod
+    def wikipedia_API(cls, adress):
+
+        first_part_of_the_adress = ''
+        
+        # We only extract the first part of the adress, wich one we want to send to wikipedia API. 
+        # This fisrt part is before the first comma...
+        try:
+            for letter in adress:
+                if letter != ',':
+                    # We don't want the digits, it would disturb the Wiki API...
+                    if not letter.isdigit():
+                        first_part_of_the_adress += letter
+                else:
+                    break
+
+            summary = wikipedia.summary(first_part_of_the_adress)                            # We get the summary...
+            page = wikipedia.page(first_part_of_the_adress)                                  # ... then stock the page in a variable...
+            url_to_the_page = page.url                                                       # ... to finally get the url of this wiki's api object.
+            
+            response = {"summary": summary, "url": url_to_the_page}
+            
+            return response
+        
+        except:
+
+            return "ERROR : We couldn't find more informations on wikipedia for this adress..."
--- views.py
+++ views.py
@@ -1,56 +1,33 @@
 from flask import Flask, render_template, url_for, request, jsonify
-from flask_googlemaps import GoogleMaps, Map
+import googlemaps
+from datetime import datetime
 
 import static
 import config
+from utils import Process
 
 app = Flask(__name__)
 app.config.from_object('config')
 
-# Set key as config
-app.config["GoogleMaps_Key"] = config.MAPS_API_KEY
-
-# Initialize the extension
-GoogleMaps(app)
+# Set an instance of Google Maps
+gmaps = googlemaps.Client(key=config.MAPS_API_KEY)
 
 
 @app.route('/')
 @app.route('/index/')
 def index():
+    
+    dictionary = {'lat': 46.3458880, 'lng': 2.432046}
 
-    return render_template('index.html')
-
+    return render_template('index.html', lat=dictionary['lat'], lng=dictionary['lng'], zoom=5, marker=0)
 
 @app.route('/mapview')
 def mapview():
-    # creating a map in the view
-    mymap = Map(
-        identifier="view-side",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[(37.4419, -122.1419)]
-    )
-    sndmap = Map(
-        identifier="sndmap",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-             'lat': 37.4419,
-             'lng': -122.1419,
-             'infobox': "<b>Hello World</b>"
-          },
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-             'lat': 37.4300,
-             'lng': -122.1400,
-             'infobox': "<b>Hello World from other place</b>"
-          }
-        ]
-    )
-    return render_template('exemple.html', mymap=mymap, sndmap=sndmap)
-
+    
+    # Geocoding an address
+    geocode_result = gmaps.geocode('Aéroport de Paris-Charles de Gaulle')
+    coordinates = geocode_result[0]['geometry']['location']
+    return render_template('index.html', lat=coordinates['lat'], lng=coordinates['lng'], zoom=12, marker=1)
 
 @app.route('/chatProcess', methods= ['POST'])
 def chatProcess():
@@ -59,6 +36,30 @@ def chatProcess():
     output = userQuestion
 
     if userQuestion:
+        
+        # We process the question in the parser
+        # We stock it in a variable
+        question_parsed = Process.parser(userQuestion)
+        print(question_parsed)
+
+        # We call the Google Maps API with the result of the parser
+        # We stock the answer in a variable
+        adress = Process.google_maps_API(question_parsed)['adress']
+        coordinates = Process.google_maps_API(question_parsed)['coordinates']
+
+        print('Here is the adress :', adress)
+        print('Here are the coordinates :', coordinates)
+
+        # We call the Wiki API with the adress returned by Google Maps
+        # We stock the answer in a variable
+        summary = Process.wikipedia_API(adress)['summary']
+        url = Process.wikipedia_API(adress)['url']
+
+        print("Here is the summary :", summary)
+        print("Here is the url :", url)
+
+        # We return all the results in a JSON file to display correctly with ajax in the template
+        
         return jsonify({'output': output})                                     # The return jsonify( {‘output’:output} ) will return output as a json data
 
     else:
